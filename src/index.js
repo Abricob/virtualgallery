2b94b229a21ab86fa977aa400f7076772404106e
Abricob <mayeul.ledore.pro@gmail.com>
2021-08-16 10:26:01 +0200

    new

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)) {
-	document.body.innerText = "This project doesn't work properly on mobile devices";
+	document.body.innerText = "Merci d'utiliser un ordinateur pour afficher le visuel";
 	throw new Error("mobile device");
 }
 
